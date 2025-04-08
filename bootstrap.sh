497555684013e29c5be43ddaf360f7d055048d8a
David <DavidDubovik19@gmail.com>
2025-04-08 19:57:54 +0300

    fix 1

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kind create cluster --config cluster.yml # create work cluster on the configuration file cluster.yml.
 kubectl taint nodes -l app=mysql app=mysql:NoSchedule # Applies a taint (a restriction) to all nodes with the label app=mysql
-helm install todoapp .infrastructure/helm-chart/todoapp # Installs a Helm chart named todoapp from the local directory helm-chart/todoapp.
+helm install todoapp helm-chart/todoapp # Installs a Helm chart named todoapp from the local directory helm-chart/todoapp.
 kubectl get all,cm,secret,ing -A > output.log # Fetches information about all resources across all namespaces (-A) and saves it to a file named output.log.
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
