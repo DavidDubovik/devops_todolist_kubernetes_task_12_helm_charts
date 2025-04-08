93218ce7d42f08f8493f6f469ef97d24dfb49b2d
David <DavidDubovik19@gmail.com>
2025-04-08 19:55:32 +0300

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-kubectl apply -f .infrastructure/mysql/ns.yml
-kubectl apply -f .infrastructure/mysql/configMap.yml
-kubectl apply -f .infrastructure/mysql/secret.yml
-kubectl apply -f .infrastructure/mysql/service.yml
-kubectl apply -f .infrastructure/mysql/statefulSet.yml
-
-kubectl apply -f .infrastructure/app/ns.yml
-kubectl apply -f .infrastructure/app/pv.yml
-kubectl apply -f .infrastructure/app/pvc.yml
-kubectl apply -f .infrastructure/app/secret.yml
-kubectl apply -f .infrastructure/app/configMap.yml
-kubectl apply -f .infrastructure/app/clusterIp.yml
-kubectl apply -f .infrastructure/app/nodeport.yml
-kubectl apply -f .infrastructure/app/hpa.yml
-kubectl apply -f .infrastructure/app/deployment.yml
-
+kind create cluster --config cluster.yml # create work cluster on the configuration file cluster.yml.
+kubectl taint nodes -l app=mysql app=mysql:NoSchedule # Applies a taint (a restriction) to all nodes with the label app=mysql
+helm install todoapp .infrastructure/helm-chart/todoapp # Installs a Helm chart named todoapp from the local directory helm-chart/todoapp.
+kubectl get all,cm,secret,ing -A > output.log # Fetches information about all resources across all namespaces (-A) and saves it to a file named output.log.
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 # kubectl apply -f .infrastructure/ingress/ingress.yml
